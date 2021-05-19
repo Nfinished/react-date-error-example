1eec2ab614b28822520fd2822b49d88e03afeeb6
Adam Trager <atrager@drift.com>
2021-05-19 09:25:27 -0400

    example

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import 'react-dates/initialize'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { DateRangePicker } from 'react-dates'
+
+const MyComponent = () => {
+  return (
+    <DateRangePicker />
+  )
+}
+
+ReactDOM.render(
+  <div>hi</div>,
+  document.getElementById('root')
+)
